39666ba82704c8c4374e4d0699c0cfcd88c7cbb1
dxfusse <dxfusse@gmail.com>
2026-01-15 11:41:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,15 +12,64 @@ fetch('/entering', {
   },
   body: JSON.stringify({user : userInfo})
 })
+
+document.getElementById('app_profile').style.opacity = 0;
+document.getElementById('app_prognoz').style.opacity = 0;
+document.getElementById('global_top').style.opacity = 0;
+document.getElementById('helper').style.opacity = 0;
+
 document.getElementById('profile').onclick = () => {
-  location.href = 'profile.html';
+  document.getElementById('app_profile').style.opacity = 1;
+  document.getElementById('app_prognoz').style.opacity = 0;
+  document.getElementById('app_global_top').style.opacity = 0;
+  document.getElementById('app_helper').style.opacity = 0;
+  document.getElementById('app_main').style.opacity = 0;
 };
 document.getElementById('prognoz').onclick = () => {
-  location.href = 'prognoz.html';
+  document.getElementById('app_profile').style.opacity = 0;
+  document.getElementById('app_prognoz').style.opacity = 1;
+  document.getElementById('app_global_top').style.opacity = 0;
+  document.getElementById('app_helper').style.opacity = 0;
+  document.getElementById('app_main').style.opacity = 0;
 };
 document.getElementById('global_top').onclick = () => {
-  location.href = 'global_top.html';
+  document.getElementById('app_profile').style.opacity = 0;
+  document.getElementById('app_prognoz').style.opacity = 0;
+  document.getElementById('app_global_top').style.opacity = 1;
+  document.getElementById('app_helper').style.opacity = 0;
+  document.getElementById('app_main').style.opacity = 0;
 };
 document.getElementById('helper').onclick = () => {
-  location.href = 'global_top.html';
+  document.getElementById('app_profile').style.opacity = 0;
+  document.getElementById('app_prognoz').style.opacity = 0;
+  document.getElementById('app_global_top').style.opacity = 0;
+  document.getElementById('app_helper').style.opacity = 1;
+  document.getElementById('app_main').style.opacity = 0;
 };
+
+//Раздел для profile
+const userid = tg.initDataUnsafe.user.id;
+
+fetch('/getUserInfo', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+        body: JSON.stringify({userid : userid})
+})
+.then(response => response.json())
+.then(data =>{
+    document.getElementById('name').innerText = data.username;
+    document.getElementById('id').innerText = tg.initDataUnsafe.user.id;
+    document.getElementById('score').innerText = data.score       
+})
+
+document.getElementById('back_profile').onclick = () => {
+  document.getElementById('app_profile').style.opacity = 0;
+  document.getElementById('app_prognoz').style.opacity = 0;
+  document.getElementById('app_global_top').style.opacity = 0;
+  document.getElementById('app_helper').style.opacity = 0;
+  document.getElementById('app_main').style.opacity = 1;
+};
+
+
