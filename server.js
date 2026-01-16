45c9fdcc3287badb7923eefb317ca7d1b0132f71
dxfusse <dxfusse@gmail.com>
2026-01-16 07:50:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ initDatabase();
 app.post('/entering', (req, res) => {
   console.log('Заход нового пользователя');
   const info = req.body.user;
-  console.log('Получены данные: ' + info)
+  console.log('Получены данные: ', info)
 
 //  const exists = database.users.some(u => u.id === uid);
 //  if (!exists) {
@@ -127,6 +127,7 @@ app.listen(PORT, () => {
   console.log(`Сервер запущен на порту: ${PORT}`);
   console.log(`Mini App доступен на /`);
 });
+
 
 
 
