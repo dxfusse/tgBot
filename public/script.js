6e58294f8d51cda82033edeb709a3a9d2da83934
dxfusse <dxfusse@gmail.com>
2026-01-16 07:41:44 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ function render(html) {
 // Main Page
 function MainPage() {
   const user = tg.initDataUnsafe.user;
-
+  alert(user);
   fetch('https://tgbot-eiq1.onrender.com/entering', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -82,5 +82,6 @@ function bindEvents() {
 
 // Старт приложения
 go('main');
+
 
 
