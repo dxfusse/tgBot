108f658341cc0913ab956155e8f7a8a8c044c8e2
dxfusse <dxfusse@gmail.com>
2026-01-18 09:25:07 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -21,7 +21,7 @@ function MainPage() {
     body: JSON.stringify({ user : user })
   });
 
-  return `
+  render( `
     <img src="f1_logo.png" class="f1_logo">
     <p class="текст-мэйн">Привет! Я твой помощник в составлении <br>прогнозов на каждую гонку в Формуле 1. Если ты угадаешь топ-10, то получишь баллы!</p>
     <p class="меню-текст">Доступные действия</p>
@@ -31,7 +31,14 @@ function MainPage() {
       <button data-page="global_top" class="кнопка-меню">Глобальный топ</button>
       <button data-page="helper" class="кнопка-меню" id="helper">Помощник</button>
     </div>
-  `;
+  `);
+
+  const menuButtons = document.querySelectorAll('.кнопка-меню');
+  menuButtons.forEach((btn, index) => {
+    setTimeout(()=>{
+        btn.classList.add('activate-menuButtons-animation')
+    }, index * 70)
+  })
 }
 
 
@@ -40,20 +47,28 @@ function ProfilePage() {
   const user = tg.initDataUnsafe.user;
 
   render(`
-    <img src="f1_logo.png" class="f1_logo">
     <p class="меню-текст">Твой профиль</p>
+    <img class="pfp" id="pfp">
     <div class="profile-menu">
-      <img class="pfp" id="pfp">
+      <p class="текст-меню" id="id">Загрузка...</p>
+      <p class="текст-меню" id="username">Загрузка...</p>
+      <p class="текст-меню" id="name">Загрузка...</p>
+      <p class="текст-меню" id="score">Загрузка...</p>
       <div class="меню">
-        <p class="текст-меню" id="id">Загрузка...</p>
-        <p class="текст-меню" id="username">Загрузка...</p>
-        <p class="текст-меню" id="name">Загрузка...</p>
-        <p class="текст-меню" id="score">Загрузка...</p>
+        <button data-page="my_team" class="кнопка-меню">Моя команда</button>
+        <button data-page="statistic" class="кнопка-меню">Статистика</button>
       </div>
     </div>
     <p></p>
     <button class="кнопка-меню" data-page="main">Назад</button>
   `);
+
+  const menuButtons = document.querySelectorAll('.текст-меню');
+  menuButtons.forEach((btn, index) => {
+    setTimeout(()=>{
+        btn.classList.add('activate-menuButtons-animation')
+    }, index * 70)
+  })
 
   fetch('https://tgbot-eiq1.onrender.com/getUserInfo', {
     method: 'POST',
@@ -73,7 +88,7 @@ function ProfilePage() {
 
 // SPA Маршрутизация
 function go(page) {
-  if (page === 'main') render(MainPage());
+  if (page === 'main') MainPage();
   if (page === 'profile') ProfilePage();
 }
 
